b4571b8f36801cf6cd8d8f1af913de8628004356
Axel Araujo Lynch <AxelAraujoLynch@hotmail.com>
2023-08-14 19:09:35 -0300

    Arreglos para la entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import NavBarComponent from './components/NavBarComponent/NavBarComponent';
 
 
 const App = ()  => {
 
 return (
  
-       <div >
-       
+       <div>
+       <NavBarComponent/>
+
+       {
+
+
+
+
+
+       }
+
        
        </div>
 
